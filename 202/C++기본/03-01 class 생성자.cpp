931b2f031663058d123a70d8265ca4eec590d5ae
jihye1012 <yjh0510122@gmail.com>
2022-04-20 09:48:00 +0900

    멤버함수를 class 바깥에 정의

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -6,23 +6,35 @@ using namespace std;
 class Student
 {
 private:
-	int sHakbun;
+	int nHakbun;
 	string sName;
 
 	//생성자 : 객체가 생성될 때 호출되는 함수
 public:
-	Student(void)
-	{
-		sHakbun = 1234;
-		sName = "이사랑";
-		cout << "학번이 등록되었습니다" << endl;
-	}
-	void show(void)
-	{
-		cout << "학번은 " << sHakbun << "입니다" << endl;
-		cout << "이름은 " << sName << "입니다" << endl;
-	}
+	Student(void);
+	void show(void);
+
 };
+//class Student
+//{
+//private:
+//	int sHakbun;
+//	string sName;
+//
+//	//생성자 : 객체가 생성될 때 호출되는 함수
+//public:
+//	Student(void)
+//	{
+//		sHakbun = 1234;
+//		sName = "이사랑";
+//		cout << "학번이 등록되었습니다" << endl;
+//	}
+//	void show(void)
+//	{
+//		cout << "학번은 " << sHakbun << "입니다" << endl;
+//		cout << "이름은 " << sName << "입니다" << endl;
+//	}
+//};
 
 int main(void)
 {
@@ -30,4 +42,15 @@ int main(void)
 	Student stu2 = Student();
 	stu2.show();
 	return 0;
+}
+Student::Student(void)
+{
+	nHakbun = 1234;
+	sName = "이사랑";
+	cout << "학번이 등록되었습니다" << endl;
+}
+void Student::show(void)
+{
+	cout << "학번은 " << nHakbun << "입니다" << endl;
+	cout << "이름은 " << sName << "입니다" << endl;
 }
