a65de9840e5acab92edad10b664cc3a9c1ff07d7
jihye1012 <yjh0510122@gmail.com>
2022-04-20 09:11:19 +0900

    객체 기본생성(default 생성자)

---
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+private:
+	int sHakbun;
+	string sName;
+
+	//생성자 : 객체가 생성될 때 호출되는 함수
+	//디폴트 생성자를 컴파일러가 만들어준다.
+	//Student(){}
+};
+
+int main(void)
+{
+	Student stu1;
+	Student stu2 = Student();
+
+	return 0;
+}
