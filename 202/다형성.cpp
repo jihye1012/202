e2e12903a33c3e4b1617177ceb6da73a8947dddf
jihye1012 <yjh0510122@gmail.com>
2022-08-31 09:55:43 +0900

    virtual 소멸자를 사용하지 않으면 메모리 누수가 일어난다

--- 202/다형성.cpp
+++ 202/다형성.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 class Animal {
 public:
+	Animal() { cout << "»ý¼º" << endl; }
+	~Animal() { cout << "¼Ò¸ê" << endl; }
 	void eat(void) { cout << "µ¿¹°¸ÔÀÌ" << endl; }
 	virtual void roar(void) { cout << "µ¿¹°Â¢¾î" << endl; }
 	void walk(void) { cout << "µ¿¹°°É¾î" << endl; }
@@ -17,11 +19,15 @@ private:
  
 class Tiger : public Animal {
 public:
+	Tiger() { cout << "»ý¼º" << endl; }
+	~Tiger() { cout << "¼Ò¸ê" << endl; }
 	void roar(void) { cout << "¾îÈï" << endl; }
 };
 
 class Dog : public Animal {
 public:
+	Dog() { cout << "»ý¼º" << endl; }
+	~Dog() { cout << "¼Ò¸ê" << endl; }
 	void roar(void) { cout << "¸Û¸Û" << endl; }
 };
 
@@ -30,13 +36,16 @@ void main(void)
 	//Animal animal;
 	//animal.roar();
 
-	Animal* animal = new Animal();
-	animal->roar();
-	delete animal;
+	//Animal* animal = new Animal();
+	//animal->roar();
+	//delete animal;
 
-	Dog* dog = new Dog();
-	dog->roar();
-	delete dog;
+	//Dog* dog = new Dog();
+	//dog->roar();
+	//delete dog;
+
+	Animal* animal = new Tiger();
+	delete animal;
 		
 	//Dog dog;
 	//dog.roar();
