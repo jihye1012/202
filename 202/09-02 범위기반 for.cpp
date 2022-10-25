95b77532b4d0ee3b13c06c71dcc21e096ccf4a24
jihye1012 <yjh0510122@gmail.com>
2022-10-26 08:50:33 +0900

    범위기반 for문

--- 202/09-02 범위기반 for.cpp
+++ 202/09-02 범위기반 for.cpp
@@ -6,13 +6,8 @@ using namespace std;
 int main(void)
 {
 	double arr[4] = { 1.1,2.2,3.3,4.4 };
-	for (int i = 0; i < 4; i++) {
-		cout << arr[i] << " ";
-	}
-	//전통적인 for
-	
-	//for (double x : arr) {
-	//	cout << x << " ";
-	//} //범위기반 for
+	for (double x : arr) {
+		cout << x << " ";
+	} //범위기반 for
 	return 0;
 }
