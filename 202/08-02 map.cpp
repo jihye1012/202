239243317ed3a1240ab52f11d82481d18d3d1688
jihye1012 <yjh0510122@gmail.com>
2022-10-26 09:01:50 +0900

    auto로 간편하게

--- 202/08-02 map.cpp
+++ 202/08-02 map.cpp
@@ -20,13 +20,14 @@ int main(void)
 	m["써니"] = "유인나";
 	//m.insert(pair<string, string>("김신", "공유"));
 
+	
 
-	map<string,string> ::iterator iter;
+	//map<string,string> ::iterator iter;
 
 	//벡터의 원소들을 출력 (iterator)
 	//삽입할때마다 key는 자동정렬
 
-	for (iter = m.begin(); iter != m.end(); iter++)
+	for (auto iter = m.begin(); iter != m.end(); iter++)
 	{
 		cout << iter->first << "역 " << iter->second << "분" << endl;
 		//cout << (*iter).first << "역 " << (*iter).second << "분" << endl;
